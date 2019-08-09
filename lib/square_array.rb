3bb2de1006c652276ddfecb400e4c1bf8f98821e
Mark Bihag <markbihag330@gmail.com>
2019-08-09 05:23:21 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
   # your code here
+  counter = 0
+    while array[counter] < array.length do
+      array.delete(counter)
+      array.insert(counter, array[counter] ** 2)
+      counter++
+  return new_numbers
 end
